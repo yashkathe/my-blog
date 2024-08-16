560eb75e0243180741953613463c678bac25d296
Yash Kathe <yashkathe123@gmail.com>
2024-08-16 11:41:17 +0530

    created custom http hook for fetching data

--- src/Components/Blog/Blogs.jsx
+++ src/Components/Blog/Blogs.jsx
@@ -3,49 +3,27 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 import BlogPreview from "./BlogPreview";
-
-import { SearchContext } from "../../Context/SearchContext";
-
-import styles from "./Blogs.module.css";
 import ErrorComponent from "../../error/ErrorComponent";
 import Spinner from "../UI/Spinner";
+import { SearchContext } from "../../Context/SearchContext";
+import useHttpHook from "../../Hooks/useHttpHook";
+
+import styles from "./Blogs.module.css";
 
 const Blogs = () => {
-	const [isLoading, setIsLoading] = useState(true);
-	const [hasError, setError] = useState(false);
-	const [data, setData] = useState();
-	const [pageNumber, setPageNumber] = useState(1);
-
 	const { searchTerm } = useContext(SearchContext);
-	const [previewBlogs, setPreviewBlogs] = useState([]);
 
-	const fetchData = async (pageNumber) => {
-		try {
-			setIsLoading(true);
-			const response = await axios.get(`http://127.0.0.1:8000/api/get-blog/${pageNumber}`);
-			setData(response.data);
-			setPreviewBlogs(response.data.data);
-			setIsLoading(false);
-		} catch (err) {
-			setError(err);
-			setIsLoading(false);
-		}
-	};
-
-	// fetch data
-	useEffect(() => {
-		fetchData(pageNumber);
-	}, [pageNumber]);
+	const { data, error, isLoading } = useHttpHook("get-blog/");
 
 	return (
 		<>
 			<div className={styles.parent}>
-				{!isLoading && !hasError && previewBlogs && (
+				{!isLoading && !error && data && (
 					<div className={styles.blogs}>
-						{previewBlogs.map((blog) => (
+						{data.map((blog) => (
 							<BlogPreview
 								key={blog.id}
-                                to={`blog/${blog.id}`}
+								to={`blog/${blog.id}`}
 								title={blog.title}
 								description={blog.description}
 								tags={blog.tags}
@@ -55,24 +33,25 @@ const Blogs = () => {
 				)}
 
 				{/* no data found for search results */}
-				{previewBlogs.length === 0 && searchTerm.length > 0 && !isLoading && (
+				{!isLoading && data && data.length === 0 && searchTerm.length > 0 && (
 					<p>No results found for: {searchTerm}</p>
 				)}
 
 				{/* database is empty */}
-				{previewBlogs.length === 0 &&
-					!hasError &&
-					searchTerm.length === 0 &&
-					!isLoading && <p>No Blogs Uploaded Yet</p>}
+				{!isLoading &&
+					data &&
+					data.length === 0 &&
+					!error &&
+					searchTerm.length === 0 && <p>No Blogs Uploaded Yet</p>}
 
 				{/* data is loading */}
-				{isLoading && <Spinner />}
+				{isLoading && !data && <Spinner />}
 
 				{/* something went wrong */}
-				{hasError && !isLoading && (
+				{!isLoading && error && (
 					<ErrorComponent
-						name={`${hasError.name} ` || "Error"}
-						message={hasError.message || "Something Went Wrong"}
+						name={`${error.name} ` || "Error"}
+						message={error.message || "Something Went Wrong"}
 					/>
 				)}
 			</div>
